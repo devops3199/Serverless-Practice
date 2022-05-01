c73d3ec204b326df5afe8f851c5d2dc40131d5e5
Ray Jeong <devops3199@gmail.com>
2022-05-01 14:05:40 +0900

    feat: create note

--- prac3/src/App.js
+++ prac3/src/App.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useReducer } from "react";
+import { v4 as uuid } from "uuid";
 import { API } from "aws-amplify";
 import { listNotes } from "./graphql/queries";
+import { createNote } from "./graphql/mutations";
+
+const CLIENT_ID = uuid();
 
 const initialState = {
   notes: [],
@@ -11,6 +15,12 @@ const initialState = {
 
 const reducer = (state, action) => {
   switch (action.type) {
+    case "ADD_NOTE":
+      return { ...state, notes: [action.note, ...state.notes] };
+    case "RESET_FORM":
+      return { ...state, form: initialState.form };
+    case "SET_INPUT":
+      return { ...state, form: { ...state.form, [action.name]: action.value } };
     case "SET_NOTES":
       return { ...state, notes: action.notes, loading: false };
     case "ERROR":
@@ -23,30 +33,83 @@ const reducer = (state, action) => {
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const handleFetch = async () => {
+  useEffect(() => {
+    async function init() {
+      try {
+        const notesData = await API.graphql({
+          query: listNotes,
+        });
+        dispatch({ type: "SET_NOTES", notes: notesData.data.listNotes.items });
+      } catch (err) {
+        console.error("error: ", err);
+        dispatch({ type: "ERROR" });
+      }
+    }
+
+    init();
+  }, []);
+
+  const handleNoteCreate = async () => {
+    const { form } = state;
+
+    if (!form.name || !form.description) {
+      return alert("Please enter a name and description");
+    }
+
+    const note = { ...form, clientId: CLIENT_ID, completed: false, id: uuid() };
+    dispatch({ type: "ADD_NOTE", note });
+    dispatch({ type: "RESET_FORM" });
+
     try {
-      const notesData = await API.graphql({
-        query: listNotes,
+      await API.graphql({
+        query: createNote,
+        variables: { input: note },
       });
-      dispatch({ type: "SET_NOTES", notes: notesData.data.listNotes.items });
+      console.log("Successfully created note!");
     } catch (err) {
       console.error("error: ", err);
-      dispatch({ type: "ERROR" });
     }
   };
 
-  useEffect(() => {
-    handleFetch();
-  }, []);
-
   return (
-    <div style={{ padding: 20 }}>
-      {state.notes.map((note) => (
-        <div>
-          <h3>{note.name}</h3>
-          <p>{note.description}</p>
-        </div>
-      ))}
+    <div>
+      <div>
+        <input
+          type="text"
+          placeholder="Note Name"
+          name="name"
+          value={state.form.name}
+          onChange={(e) =>
+            dispatch({
+              type: "SET_INPUT",
+              name: e.target.name,
+              value: e.target.value,
+            })
+          }
+        />
+        <input
+          type="text"
+          placeholder="Note Description"
+          name="description"
+          value={state.form.description}
+          onChange={(e) =>
+            dispatch({
+              type: "SET_INPUT",
+              name: e.target.name,
+              value: e.target.value,
+            })
+          }
+        />
+        <button onClick={handleNoteCreate}>Create Note</button>
+      </div>
+      <div style={{ padding: 20 }}>
+        {state.notes.map((note) => (
+          <div>
+            <h3>{note.name}</h3>
+            <p>{note.description}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
